d009db36bafe5518f3f4b336d953cbfd9533773c
Stefan Adolf <stefan.adolf@turbinekreuzberg.com>
2019-09-25 16:23:55 +0200

    projection

--- bin/project-current-checked-in-users.php
+++ bin/project-current-checked-in-users.php
@@ -4,10 +4,14 @@
 namespace Building\App;
 
 use Building\Domain\Aggregate\Building;
+use Building\Domain\Command\RegisterNewBuilding;
+use Building\Domain\DomainEvent\UserCheckedIn;
+use Building\Domain\DomainEvent\UserCheckedOut;
 use Interop\Container\ContainerInterface;
 use Prooph\EventSourcing\AggregateChanged;
 use Prooph\EventStore\EventStore;
 use Prooph\EventStore\Stream\StreamName;
+use function var_dump;
 
 (static function () {
     /** @var ContainerInterface $dic */
@@ -17,13 +21,19 @@ use Prooph\EventStore\Stream\StreamName;
 
     /** @var AggregateChanged[] $history */
     $history = $eventStore->loadEventsByMetadataFrom(new StreamName('event_stream'), [
-        'aggregate_name' => Building::class,
+        'aggregate_type' => Building::class,
     ]);
 
-    $usersInBuildings = [];
+    $buildings = [];
 
     foreach ($history as $event) {
-        // ... ???
+        if($event instanceof RegisterNewBuilding) {
+            $buildings[$event->aggregateId()] = [];
+        } elseif ($event instanceof UserCheckedIn) {
+            $buildings[$event->building()->toString()][$event->username()] = 1;
+        } elseif ($event instanceof UserCheckedOut) {
+            unset ($buildings[$event->building()->toString()][$event->username()]);
+        }
     }
 
     \array_walk($usersInBuildings, static function (array $users, string $buildingId) {
